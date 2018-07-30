63083ea334917b4137b032c6fab9564fc3cefbe6
Gianni Perez <gianniperez@gmail.com>
2018-07-29 20:48:13 -0400

    variable(s) renaming

--- shannon-entropy.py
+++ shannon-entropy.py
@@ -26,19 +26,15 @@ def shannon_entropy(data):
     return symbol_frequency(stack)
 
 
-def symbol_frequency(symbolset):
-    bitset = [round(symbolset[symbol] * math.log2(symbolset[symbol]), 5) for symbol in symbolset]
-    entropy = -1 * (round(sum(bitset), 5))
+def symbol_frequency(symbol_set):
+    bit_set = [round(symbol_set[symbol] * math.log2(symbol_set[symbol]), 5) for symbol in symbol_set]
+    entropy = -1 * (round(sum(bit_set), 5))
     return entropy
 
 
-def main():
+if __name__ == '__main__':
     m = input('\nEnter the message: ')
     bits = shannon_entropy(m)
     print('\nH(X) = {0} bits. Rounded to {1} bits/symbol, '.format(bits, round(bits)))
     print('it will take {0} bits to optimally encode "{1}"'.format(len(m) * round(bits), m))
     print('\nMetric entropy: %.5f' % (bits / len(m)))
-
-
-if __name__ == '__main__':
-    main()
